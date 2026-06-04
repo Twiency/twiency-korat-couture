3116f4dd977404acb31076929b86f80276cf7b30
Twiency <55455947+Twiency@users.noreply.github.com>
2026-06-05 00:27:03 +0530

    Add dresses data array to data.js

---
+++ data.js
@@ -0,0 +1,19 @@
+const dresses = [
+
+{
+name: "Celeste Rose Lehenga",
+image: "dresses/dress1.jpg",
+price: "₹20,000",
+description:
+"Luxury rose-toned couture lehenga with shimmer elegance."
+},
+
+{
+name: "Ivory Pearl Couture Lehenga",
+image: "dresses/dress2.jpg",
+price: "₹20,000",
+description:
+"Elegant ivory lehenga with premium embroidery."
+}
+
+];
