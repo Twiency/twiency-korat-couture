52d892f5a6df6b8c5a9c4bbd6d4d79dfe789569d
Twiency <55455947+Twiency@users.noreply.github.com>
2026-06-05 00:41:09 +0530

    Refactor image paths in data.js
    Updated image paths for dresses to use single files.

--- data.js
+++ data.js
@@ -9,9 +9,7 @@ description:
 "Luxury rose-toned couture lehenga crafted for elegant celebrations.",
 
 images: [
-"dresses/dress1-1.jpg",
-"dresses/dress1-2.jpg",
-"dresses/dress1-3.jpg"
+"dresses/dress1.jpg"
 ]
 },
 
@@ -24,8 +22,7 @@ description:
 "Elegant ivory couture lehenga with premium embroidery and regal styling.",
 
 images: [
-"dresses/dress2-1.jpg",
-"dresses/dress2-2.jpg"
+"dresses/dress2.jpg"
 ]
 }
 
