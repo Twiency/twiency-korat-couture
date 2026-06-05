756585d287e55106dd7794f4d62aaafd72717773
Twiency <55455947+Twiency@users.noreply.github.com>
2026-06-05 07:34:55 +0530

    Update image paths in data.js

--- data.js
+++ data.js
@@ -4,10 +4,12 @@ const dresses = [
 name: "Celeste Rose Lehenga",
 category: "Reception",
 price: "₹20,000",
+
 description:
 "Luxury rose-toned couture lehenga crafted for elegant celebrations.",
+
 images: [
-"../dresses/dress1.jpg"
+"dresses/dress1.jpg"
 ]
 },
 
@@ -15,10 +17,12 @@ images: [
 name: "Ivory Pearl Couture Lehenga",
 category: "Bridal",
 price: "₹20,000",
+
 description:
 "Elegant ivory couture lehenga with premium embroidery and regal styling.",
+
 images: [
-"../dresses/dress2.jpg"
+"dresses/dress2.jpg"
 ]
 }
 
